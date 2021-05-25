7adb92f73bd954e98667c95625dc48b5a8a22e4c
marco.spasiano <marco.spasiano@cnr.it>
2021-05-25 07:54:24 +0200

    FIX contesto ACE

--- src/main/java/it/cnr/si/security/JWTAuthenticationManager.java
+++ src/main/java/it/cnr/si/security/JWTAuthenticationManager.java
@@ -81,7 +81,6 @@ public class JWTAuthenticationManager implements AuthenticationManager {
         List<BossDto> bossDtos = aceService.ruoliUtenteAttivi(principal);
         authorities.addAll(
             bossDtos.stream()
-                .filter(bossDto -> bossDto.getRuolo().getContesto().getSigla().equals(contestoACE))
                 .filter(bossDto -> {
                     return !(bossDto.getEntitaOrganizzativa() != null && bossDto.getRuolo().getTipoRuolo().equals(TipoRuolo.ROLE_ADMIN));
                 })
@@ -92,7 +91,6 @@ public class JWTAuthenticationManager implements AuthenticationManager {
                 .collect(Collectors.toList()));
 
         Stream<SimpleEntitaOrganizzativaWebDto> entitaOrganizzativaAssegnata = bossDtos.stream()
-            .filter(bossDto -> bossDto.getRuolo().getContesto().getSigla().equals(contestoACE))
             .filter(bossDto -> Optional.ofNullable(bossDto.getEntitaOrganizzativa()).isPresent())
             .map(bossDto -> bossDto.getEntitaOrganizzativa());
 
