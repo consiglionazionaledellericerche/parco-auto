3c4ed5573130a3b4c7f1130c0f063108231edc68
marco.spasiano <marco.spasiano@cnr.it>
2022-11-16 18:00:48 +0100

    Inserted sorting by institution in the vehicle printout

--- src/main/java/it/cnr/si/web/rest/VeicoloResource.java
+++ src/main/java/it/cnr/si/web/rest/VeicoloResource.java
@@ -43,6 +43,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -305,7 +306,7 @@ public class VeicoloResource {
                 cdSUO,
                 false);
         } else {
-            veicoliAll = veicoloRepository.findAll();
+            veicoliAll = veicoloRepository.findAll(Sort.by("istituto"));
         }
 
         DateTimeFormatter formatter =
